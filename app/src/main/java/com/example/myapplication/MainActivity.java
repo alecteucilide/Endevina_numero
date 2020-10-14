50424c4c3c72a0ba51427e346ddfa890a555a202
alecteucilide <alecadrianteucilide@gmail.com>
2020-10-14 15:25:37 +0200

    house commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -71,13 +69,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void callRanking(View view){
-        if(!name.equals("")){
+        if(!name.equals("") || !name.isEmpty()){
         Intent intent = new Intent(this, MainActivity_ranking.class);
         intent.putExtra("intentos", numIntentos);
         intent.putExtra("name", name);
         startActivity(intent);
         }else {
-            Toast.makeText(MainActivity.this, "Nombre no valida.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Nombre no valido.", Toast.LENGTH_SHORT).show();
         }
     }
 }
